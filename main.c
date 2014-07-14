6296bc8350e0252bc2cf584293a8e6f5b029b7a5
Demcsik Márton <demcsik@gmail.com>
2014-07-14 11:53:05 +0200

    SDCARD init

--- main.c
+++ main.c
@@ -1,50 +1,56 @@
 #include <stm32f4xx.h>
 #include <stm32f4xx_spi.h>
 
-// this function initializes the SPI1 peripheral
-void init_SPI1(void){
+uint8_t received_val = 0, i;
+
+#define SD_CS_LOW()     GPIO_ResetBits(GPIOE, GPIO_Pin_7)
+#define SD_CS_HIGH()    GPIO_SetBits(SGPIOE, GPIO_Pin_7)
+
+// this function initializes the SPI2 peripheral
+
+void init_SPI2(void){
 	
 	GPIO_InitTypeDef GPIO_InitStruct;
 	SPI_InitTypeDef SPI_InitStruct;
 	
 	// enable clock for used IO pins
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 	
-	/* configure pins used by SPI1
-	 * PA5 = SCK
-	 * PA6 = MISO
-	 * PA7 = MOSI
+	/* configure pins used by SPI2
+	 * PB13 = SCK
+	 * PB14 = MISO
+	 * PB15 = MOSI
 	 */
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_6 | GPIO_Pin_5;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	GPIO_Init(GPIOB, &GPIO_InitStruct);
 	
-	// connect SPI1 pins to SPI alternate function
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_SPI1);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
+	// connect SPI2 pins to SPI alternate function
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource13, GPIO_AF_SPI2);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource14, GPIO_AF_SPI2);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource15, GPIO_AF_SPI2);
 	
 	// enable clock for used IO pins
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 	
 	/* Configure the chip select pin
-	   in this case we will use PE7 */
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7;
+	   in this case we will use PD8 */
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_8;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_Init(GPIOE, &GPIO_InitStruct);
+	GPIO_Init(GPIOD, &GPIO_InitStruct);
 	
-	GPIOE->BSRRL |= GPIO_Pin_7; // set PE7 high
+	GPIOD->BSRRH |= GPIO_Pin_8; // set PE7 (CS) low	
 	
 	// enable peripheral clock
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
 	
-	/* configure SPI1 in Mode 0 
+	/* configure SPI2 in Mode 0 
 	 * CPOL = 0 --> clock is low when idle
 	 * CPHA = 0 --> data is sampled at the first edge
 	 */
@@ -54,38 +60,136 @@ void init_SPI1(void){
 	SPI_InitStruct.SPI_CPOL = SPI_CPOL_Low;        // clock is low when idle
 	SPI_InitStruct.SPI_CPHA = SPI_CPHA_1Edge;      // data sampled at first edge
 	SPI_InitStruct.SPI_NSS = SPI_NSS_Soft | SPI_NSSInternalSoft_Set; // set the NSS management to internal and pull internal NSS high
-	SPI_InitStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_4; // SPI frequency is APB2 frequency / 4
+	SPI_InitStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_128; // SPI frequency is APB2 frequency / 256 = 328,125 hz = 328 kHz
 	SPI_InitStruct.SPI_FirstBit = SPI_FirstBit_MSB;// data is transmitted MSB first
-	SPI_Init(SPI1, &SPI_InitStruct); 
+	SPI_Init(SPI2, &SPI_InitStruct); 
 	
-	SPI_Cmd(SPI1, ENABLE); // enable SPI1
+	SPI_Cmd(SPI2, ENABLE); // enable SPI2
 }
 
 /* This funtion is used to transmit and receive data 
- * with SPI1
+ * with SPI2
  * 			data --> data to be transmitted
  * 			returns received value
  */
-uint8_t SPI1_send(uint8_t data){
+uint8_t SPI2_send(uint8_t data){
 
-	SPI1->DR = data; // write data to be transmitted to the SPI data register
-	while( !(SPI1->SR & SPI_I2S_FLAG_TXE) ); // wait until transmit complete
-	while( !(SPI1->SR & SPI_I2S_FLAG_RXNE) ); // wait until receive complete
-	while( SPI1->SR & SPI_I2S_FLAG_BSY ); // wait until SPI is not busy anymore
-	return SPI1->DR; // return received data from SPI data register
+	SPI2->DR = data; // write data to be transmitted to the SPI data register
+	while( !(SPI2->SR & SPI_I2S_FLAG_TXE) ); // wait until transmit complete
+	while( !(SPI2->SR & SPI_I2S_FLAG_RXNE) ); // wait until receive complete
+	while( SPI2->SR & SPI_I2S_FLAG_BSY ); // wait until SPI is not busy anymore
+	return SPI2->DR; // return received data from SPI data register
+}
+
+uint8_t sdSpiByte(uint8_t data)
+{
+	SPI2->DR = data;
+	while( !(SPI2->SR & SPI_I2S_FLAG_TXE) ); // wait until transmit complete
+	while( !(SPI2->SR & SPI_I2S_FLAG_RXNE) ); // wait until receive complete
+	while( SPI2->SR & SPI_I2S_FLAG_BSY ); // wait until SPI is not busy anymore
+	return SPI2->DR;
+}
+
+uint8_t sdCrc7(uint8_t* chr, uint8_t cnt, uint8_t crc)
+{
+	uint8_t i, a;
+	uint8_t Data;
+	for (a=0; a<cnt; a++)
+	{
+		Data = chr[a];
+		for (i=0; i<8; i++)
+		{
+			crc <<=1;
+			if ((Data & 0x80) ^ (crc & 0x80))
+			{crc ^=0x09;}
+			Data <<=1;
+		}
+	}
+	return crc & 0x7F;
+}
+
+void sdSendCommand(uint8_t cmd, uint32_t param)
+{
+	uint8_t send[6];
+	
+	send[0] = cmd | 0x40;
+	send[1]	= param >> 24;
+	send[2]	= param >> 16;
+	send[3]	= param >> 8;
+	send[4]	= param ;
+	send[5] = (sdCrc7(send, 5, 0) << 1) | 1;
+	for (cmd = 0; cmd < sizeof(send); cmd++)
+	{
+		SPI2_send(send[cmd]);
+	}
+}
+
+uint8_t sdReadResp(void)
+{
+  uint8_t Data = 0;
+  
+  /*!< Wait until the transmit buffer is empty */
+  while( !(SPI2->SR & SPI_I2S_FLAG_TXE) ); // wait until transmit complete
+
+  /*!< Send the byte */
+  SPI2_send(0xFF);
+
+  /*!< Wait until a data is received */
+	while( !(SPI2->SR & SPI_I2S_FLAG_RXNE) ); // wait until receive complete
+
+		/*!< Get the received data */
+  Data = SPI2->DR;
+
+  /*!< Return the shifted data */
+  return Data;
+}
+
+uint8_t sdCommandAndResponse(uint8_t cmd, uint32_t param)
+{
+	uint8_t ret, retry;
+	retry = 0;
+	SPI2_send(0xFF);
+	sdSendCommand(cmd, param);
+	while ((ret = SPI_I2S_ReceiveData(SPI2)) == 0xFF)
+	{
+		SPI2_send(0xFF);
+		if (retry++ > 100) { break; }
+	}
+	return ret;
+}
+
+void fatal(uint8_t val)
+{
+	while(1);
 }
 
 int main(void){
 	
-	uint8_t received_val = 0;
+	uint8_t v;
 	
-	init_SPI1();
+	init_SPI2();
 	
-	while(1){
+	GPIOD->BSRRL |= GPIO_Pin_8; // set PE7 (CS) high	
+	
+	for (v=0; v<30; v++)
+		{
+			SPI2_send(0xFF);  // transmit data
+		}
 		
-		GPIOE->BSRRH |= GPIO_Pin_7; // set PE7 (CS) low
-		SPI1_send(0xAA);  // transmit data
-		received_val = SPI1_send(0x00); // transmit dummy byte and receive data
-		GPIOE->BSRRL |= GPIO_Pin_7; // set PE7 (CS) high
-	}
+	GPIOD->BSRRH |= GPIO_Pin_8; // set PE7 (CS) low
+		
+	v = sdCommandAndResponse(0, 0);
+		while ( (received_val = SPI_I2S_ReceiveData(SPI2)) != 1 )
+		{
+			SPI2_send(0xFF);
+		}
+	if (v!=1) {fatal(2);}
+	v = sdCommandAndResponse(1, 0);
+		if (v!=1) {fatal(2);}	
+	
+
+	while(1)
+	{
+		SPI2_send(0xA1);  // transmit data
+	}		
 }
